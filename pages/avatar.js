cdac5594ffd8a9694c3b7067095447494c067d18
rendyk-sirclo <rendy.kurniawan@sirclo.com>
2022-06-24 15:14:50 +0700

    feat: tweak style for avatar list page

--- pages/avatar.js
+++ pages/avatar.js
@@ -9,28 +9,32 @@ const Avatar = () => {
         <title>List Avatar</title>
         <meta name="description" content="List Avatar" />
       </Head>
-      <h1>List Avatar:</h1>
-      <ul>
-        {avatars.map((avatar) => (
-          <li key={avatar._id}>
-            <Link
-              href={{
-                pathname: `/avatar/${avatar._id}`,
-                query: {
-                  name: avatar.name,
-                  photoUrl: avatar.photoUrl,
-                  position: avatar.position,
-                  gender: avatar.gender,
-                  profession: avatar.profession,
-                },
-              }}
-              as={`/avatar/${avatar._id}`}
-            >
-              <a style={{ color: "blue" }}>{avatar.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{
+        padding: "80px 0 80px 0"
+      }}>
+        <h1 style={{marginTop: 0}}>List Avatar:</h1>
+        <ul>
+          {avatars.map((avatar) => (
+            <li key={avatar._id}>
+              <Link
+                href={{
+                  pathname: `/avatar/${avatar._id}`,
+                  query: {
+                    name: avatar.name,
+                    photoUrl: avatar.photoUrl,
+                    position: avatar.position,
+                    gender: avatar.gender,
+                    profession: avatar.profession,
+                  },
+                }}
+                as={`/avatar/${avatar._id}`}
+              >
+                <a style={{ color: "blue" }}>{avatar.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
